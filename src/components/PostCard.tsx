fc4242760e527af6baba47fa043937fe0326ae1b
Vandicleia Goncalves <vangoh05@protonmail.com>
2022-04-19 13:19:55 +0200

    Solution: Version 1

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -9,6 +9,6 @@ export default function PostCard({card} : PostCardProps){
         <h2>{card.name}</h2>
         <img className={'card-Image'} src={card.image} />
         <p><h3> {card.species} </h3></p>
-        <p>{card.origin.name}, {card.location.name}</p>
+        <p>{card.origin.name}</p>
     </div>
 }
