c1cbb952ecb53bcb9284a8ded09c309eb4c41b27
Vandicleia Goncalves <vangoh05@protonmail.com>
2022-04-20 08:12:57 +0200

    Redering implementation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Card } from './model/Card';
 export default function App() {
     const [characters, setCharacters] = useState<Card[]>([]);
     const [text, setText] = useState<string>("");
-    //const [filteredCards, setFilteredCards] = useState<Card[]>([]);
+    const [inputText, setInputText] = useState<string>("");
 
     const fetchCharacters = () => {
         return fetch('https://rickandmortyapi.com/api/character')
@@ -27,16 +27,14 @@ export default function App() {
 
     const onTextChange = (event : ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value);
-        //char(characters.filter(character => character.name.includes(event.target.value)));
-        //console.log(filteredCards);
     };
 
     const filteredCharacters = () => {
-        return characters.filter(character => character.name.includes(text));
-    }
+        return characters.filter(character => character.name.includes(inputText));
+    };
 
     const onButtonClick = () => {
-        // ToDo
+        setInputText(text)
     }
 
     return (
@@ -44,29 +42,15 @@ export default function App() {
             <Title />
             <input onChange={onTextChange} />
             <button onClick={onButtonClick}>Filter cards</button>
-            <RmaOverview cards={filteredCharacters()}/>
+            {
+               filteredCharacters().length
+                   ? <RmaOverview cards={filteredCharacters()}/>
+                   :
+                   <div className={"error-msg"}>
+                       Name Does not exists!
+                       <RmaOverview cards={characters}/>
+                   </div>
+            }
         </div>
     );
 }
-
-/*
-        <div className="App">
-            <Title />
-            <input onChange={onTextChange} />
-            <button onClick={onButtonClick}>Filter cards</button>
-            <RmaOverview cards={filteredCards}/>
-        </div>*/
-
-/*
-    const [text, setText] = useState<string>("dfsgsdfg");
-    const [filteredCards, setFilteredCards] = useState<Card[]>(cards);
-
-    const onTextChange = (event : ChangeEvent<HTMLInputElement>) => {
-        setText(event.target.value);
-        setFilteredCards(cards.filter(card => card.name.includes(event.target.value)));
-        console.log(filteredCards);
-        };
-
-    const onButtonClick = () => {
-        // ToDo
-    }*/
